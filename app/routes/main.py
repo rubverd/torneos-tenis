9687a1e32eb8004c723793d0fbfe2c583d982e9b
rubverd <ruben.vercav@gmail.com>
2025-08-15 01:20:10 +0200

    Implementada la funcionalidad para mostrar el ranking, además de adecentar la página de inicio

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, render_template
+from app.db.dbconnection import Connection
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',ranking=get_ranking())
+
+def get_ranking():
+    conn = Connection()
+    ranking = conn.select(
+        "SELECT username, puntos_totales FROM jugadores ORDER BY puntos_totales DESC"
+    )
+    return ranking
