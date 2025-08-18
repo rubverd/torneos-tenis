474732cabb5760b6ecf36d488319011be7244d78
rubverd <ruben.vercav@gmail.com>
2025-08-18 21:54:27 +0200

    Instalados gunicorn y dotenv

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app import create_app
-
+import os
 app = create_app()
-
-if __name__ == "__main__":
     # Railway necesita escuchar en 0.0.0.0 y puerto que viene de $PORT
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
