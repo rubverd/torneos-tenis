853a294621fd0a16d83c2c21c9c09776a4c3c120
rubverd <ruben.vercav@gmail.com>
2025-08-18 22:01:51 +0200

    Modificación en run.py

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
 from app import create_app
-import os
+
 app = create_app()
-    # Railway necesita escuchar en 0.0.0.0 y puerto que viene de $PORT
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
