edb7b4f3a6351ab6f4d53770b04cbdf0847e52ed
rubverd <ruben.vercav@gmail.com>
2025-08-18 20:17:44 +0200

    Añadida página de error handler y también algún mensaje más en la información. Siguiente paso desplegar la demo

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from .routes import main, auth, puntuaciones
 import os
 
@@ -12,4 +12,11 @@ def create_app():
     app.register_blueprint(auth.auth_bp)
     app.register_blueprint(puntuaciones.points_edit)
 
+    register_error_handlers(app)
+
     return app
+
+def register_error_handlers(app):
+    @app.errorhandler(405)
+    def error404(error):
+        return render_template('error.html'), 405
